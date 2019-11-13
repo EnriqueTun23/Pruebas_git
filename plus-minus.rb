92da0d202d89b9a2501a547aec4813245d2d7629
Enrique Tun <quiquejesus23@hotmail.com>
2019-11-13 12:08:34 -0600

    feedback accommodate  variables

--- plus-minus.rb
+++ plus-minus.rb
@@ -5,10 +5,11 @@ require 'stringio'
 
 # Complete the plusMinus function below.
 def plusMinus(arr)
+   total_number = arr.size
+   
    positives = arr.count {|number| number.positive?}
    negatives = arr.count {|number| number.negative?}
    zeros = arr.count {|number| number.zero?}
-   total_number = arr.size
    
    puts positives.fdiv(total_number)
    puts negatives.fdiv(total_number)
