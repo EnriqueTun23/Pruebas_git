4b2c415179d804036112c05094641da6ca1851de
Enrique Tun <quiquejesus23@hotmail.com>
2019-11-13 11:57:38 -0600

    feedback variables

--- plus-minus.rb
+++ plus-minus.rb
@@ -6,8 +6,8 @@ require 'stringio'
 # Complete the plusMinus function below.
 def plusMinus(arr)
    positives = arr.count {|number| number.positive?}
-   negatives = arr.count {|number| number_negative.negative?}
-   zeros = arr.count {|numeber| number.zero?}
+   negatives = arr.count {|number| number.negative?}
+   zeros = arr.count {|number| number.zero?}
    total_number = arr.size
    
    puts positives.fdiv(total_number)
