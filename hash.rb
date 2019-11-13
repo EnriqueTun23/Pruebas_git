4e3f2bfd9d66dd198388b4467d233c1478e456eb
Enrique Tun <quiquejesus23@hotmail.com>
2019-11-13 14:04:49 -0600

    Hashes exercise repair

--- hash.rb
+++ hash.rb
@@ -1,18 +1,12 @@
 DATA = {
   enterprise: 'Homie',
-  years: 4
+  years: 4,
 }
-
 def update_info(data, key, value)
-    
-    if data.has_key?(key)
-        data[key] = value
-    else  
-        data.merge!({key => value})
-    end
-    
-    data
+  return data.merge!({key => value}) if !data.has_key?(key)
+  data[key ] = value
+  
+  data
 end
-
 
 update_info(DATA, :years, 5)
