b4484f239eb4143bfbfe1b29ea4d222786c27c41
Enrique Tun <quiquejesus23@hotmail.com>
2019-11-14 11:26:37 -0600

    Exercise refactor

---
+++ refactotor_simplified.rb
@@ -0,0 +1,28 @@
+def customer_state(session_id = nil)
+    return { state: 'chat_disabled' } if !Setting.get('chat')
+
+    reconnect(session_id)
+    
+    return { state: 'offline' } if Chat.active_agent_count([id]).zero?
+    
+    waiting_count = Chat.waiting_chat_count(id)
+    return { state: 'no_seats_available', queue: waiting_count} if waiting_count >= max_queue
+    
+    { state: 'online' }    
+end
+
+# reconnect
+def reconnect (session_id)
+    return unless chat_session = Chat::Session.find_by(session_id: session_id, state: %w[waiting running]) if session_id
+    return unless chat_session
+    return {state: 'reconnect', position: chat_session.position} elsif chat_session.state == 'waiting'
+    return unless chat_session.state == 'running'
+    user = nil
+    return unless chat_session.user_id
+    chat_user = User.lookup(id: chat_session.user_id)
+    url = nil
+    session = Chat::Session.messages_by_session_id(session_id)
+    return unless user = {name: chat_user.fullname, avatar: url,} if chat_user.image && chat_user.image != 'none'
+    url = "#{Setting.get('http_type')}://#{Setting.get('fqdn')}/api/v1/users/image/#{chat_user.image}"
+    return { state:   'reconnect', session: session, agent: user,} if session
+end
