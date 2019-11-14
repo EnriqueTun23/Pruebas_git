0d9af09a3aee05c1fe758d1b8dc104afb37532e1
Enrique Tun <quiquejesus23@hotmail.com>
2019-11-14 15:39:27 -0600

    Exercise refactor revision6

--- refactotor_simplified.rb
+++ refactotor_simplified.rb
@@ -13,27 +13,34 @@ def reconnect(session_id)
   return unless session_id
   chat_session = Chat::Session.find_by(session_id: session_id, state: %w[waiting running])
   return unless chat_session
+  
   case chat_session.state
   when 'running'
-    user = nil
-    return unless chat_session.user_id
-    chat_user = User.lookup(id: chat_session.user_id)
-    url = nil
-    comparation = imgComparation(chat_user, url)
-    return comparation if comparation
+    reconect_seccion = reconnect_session(chat_session)
+    return reconect_seccion if reconect_seccion  
   when 'waiting'
     {state: 'reconnect', position: chat_session.position,}
+
 end
 
-def snake_case(chat_user, url)
+def reconnect_session(chat_session)
+  user = nil
+  return unless chat_session.user_id
+  chat_user = User.lookup(id: chat_session.user_id)
+  url = nil
+  comparison = snake_comparison(chat_user, url)
+  return comparation if comparation
+end
+
+def snake_comparison(chat_user, url)
   return unless chat_user.image && chat_user.image != 'none'
   url = "#{Setting.get('http_type')}://#{Setting.get('fqdn')}/api/v1/users/image/#{chat_user.image}"
   user = { name: chat_user.fullname, avatar: url,}
-  get_position = get_queue_positio(user)
+  get_position = queue_position(user)
   return get_position if get_position
 end
 
-def queue_positio(user)
+def queue_position(user)
   session = Chat::Session.messages_by_session_id(session_id)
   return unless session
   {state: 'reconnect', session: session, agent: user}
