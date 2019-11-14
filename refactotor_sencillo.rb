ec621d03d8298a8869ce56e912dad37959d28a56
Enrique Tun <quiquejesus23@hotmail.com>
2019-11-14 16:04:35 -0600

    Exercise two refactor

---
+++ refactotor_sencillo.rb
@@ -0,0 +1,7 @@
+def longname
+  name = fullname
+  return unless organization_id
+  organization = Organization.lookup(id: organization_id)
+  return unless organization
+  name += "(#{organization.name})"
+end
