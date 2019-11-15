ad97d5bc376d1d734ad7dd9e00a632667f5497c3
Enrique Tun <quiquejesus23@hotmail.com>
2019-11-15 16:25:57 -0600

    Exercise ten sum array revision1

--- sum_array.rb
+++ sum_array.rb
@@ -1,14 +1,13 @@
 def sum(numbers)
   total = 0
   
-  numbers.each do |element_number|
-  element_number = element_number.to_s
-  element_reverse = element_number.reverse
-  element_position = element_number.reverse.index("0")
-  
-  next if element_number.reverse.byteslice(0).to_i == 0
-  total += element_reverse.slice(0..element_position-1).reverse.to_i
+  numbers.each do |number|
+  number = number.to_s
+  number_reverse = number.reverse
+  number_position = number.reverse.index("0")
+  next if number.reverse.byteslice(0).to_i == 0
 
+  total += number_reverse.slice(0..number_position-1).reverse.to_i
   end
   
   total
