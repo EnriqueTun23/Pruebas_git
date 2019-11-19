ff3bd1a27b6ba7372e34fd39e4919e97a7abc734
Enrique Tun <quiquejesus23@hotmail.com>
2019-11-19 17:28:26 -0600

    Exercise POO contact revision2

--- class_contact.rb
+++ class_contact.rb
@@ -1,22 +1,20 @@
 class Contact
-    def self.all
-      [
-        {name: 'Jose', last_name: 'Lopez', age: 20, gender:'Masculino'},
-        {name: 'Oscar', last_name: 'Perez', age: 21, gender: 'Masculino'},
-        {name: 'Martha', last_name: 'Sanchez', age: 22, gender: 'Femenino'} 
-      ]  
-    end    
+  def self.all
+    [
+      {name: 'Jose', last_name: 'Lopez', age: 20, gender:'Masculino'},
+      {name: 'Oscar', last_name: 'Perez', age: 21, gender: 'Masculino'},
+      {name: 'Martha', last_name: 'Sanchez', age: 22, gender: 'Femenino'} 
+    ]  
+  end    
 
-    def self.find_by(key, value, personas)
-       id_person = personas.index{|person| person.key(value) && person.values_at(key)}
-       personas[id_person]
-    end
+  def self.find_by(key, value)
+    id_person = Contact.all.index{|person| person.key(value) && person.values_at(key)}
+    Contact.all[id_person]
+  end
 end
-
 
 puts "----------------Imprimir todo los arrays ----------------"
 puts Contact.all
-personas = Contact.all
 puts "----------------Busqueda perzonalizada ----------------"
-puts Contact.find_by('age', 22, personas)
+puts Contact.find_by('age', 20)
 
